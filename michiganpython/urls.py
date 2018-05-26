7cfd808cdb202e778124ed6c036759d2b7de8f23
douglance <doug.lance@gmail.com>
2018-05-26 10:32:26 -0400

    Fixed URLs

--- michiganpython/urls.py
+++ michiganpython/urls.py
@@ -4,14 +4,7 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
-# Examples:
-# url(r'^$', 'michiganpython.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
